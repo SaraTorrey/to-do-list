d25933682ffce125e5ed87745c911062666809f6
Sara Torrey <sara.moravej@gmail.com>
2019-05-26 15:32:00 -0500

    jQuery set up and styles added.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+$("li").click(function() {
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("span").click(function ( e ) {
+    $( this ).parent().fadeOut( 500, function () {
+        $( this ).remove();
+    } );
+    event.stopPropagation();
+});
