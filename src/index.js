1b38fd2d6ba30e353220f5b7a0808df8d8293648
Sara Torrey <sara.moravej@gmail.com>
2019-05-27 16:29:09 -0500

    Complete style and jQuery effects

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("span").click(function ( e ) {
+$("ul").on("click", "span", function ( e ) {
     $( this ).parent().fadeOut( 500, function () {
         $( this ).remove();
     } );
     event.stopPropagation();
 });
+
+$( "input[type='text']" ).keypress(function(e){
+    if(e.which === 13){
+        //grabbing new to-do text from input
+        var todoText = $(this).val();
+        $( this ).val( "" );
+        //create a new li and add to ul
+        $( "ul" ).append( "<li><span><i class=\"far fa-trash-alt\"></i></span></span>" + todoText + "</li>");
+    }
+});
+
+$( ".fa-plus" ).click( function () {
+    $("input[type='text']").fadeToggle();
+} );
